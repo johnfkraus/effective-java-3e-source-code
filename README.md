e3edb8f31003ae75914c584588a4aa983f3dc5f2
jk <johnkraus3@gmail.com>
2023-08-14 07:26:21 -0400

    -m

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Effective Java, Third Edition
+
+Code requires Java 11+.
+
 ![EJ3e Book Cover](https://www.pearsonhighered.com/assets/bigcovers/0/1/3/4/0134685997.jpg)
 ## Hot News! Source code finally available on GitHub. Happy Hacking!
---
+++ src/boyarsky/chapter1/PrimitiveTypeUtil.java
@@ -0,0 +1,36 @@
+package boyarsky.chapter1;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Map;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PrimitiveTypeUtil {
+
+
+  private static final Map<Class<?>, Class<?>> WRAPPER_TYPE_MAP;
+  static {
+    WRAPPER_TYPE_MAP = new HashMap<Class<?>, Class<?>>(16);
+    WRAPPER_TYPE_MAP.put(Integer.class, int.class);
+    WRAPPER_TYPE_MAP.put(Byte.class, byte.class);
+    WRAPPER_TYPE_MAP.put(Character.class, char.class);
+    WRAPPER_TYPE_MAP.put(Boolean.class, boolean.class);
+    WRAPPER_TYPE_MAP.put(Double.class, double.class);
+    WRAPPER_TYPE_MAP.put(Float.class, float.class);
+    WRAPPER_TYPE_MAP.put(Long.class, long.class);
+    WRAPPER_TYPE_MAP.put(Short.class, short.class);
+    WRAPPER_TYPE_MAP.put(Void.class, void.class);
+  }
+  public static boolean isPrimitiveType(Object source) {
+    return WRAPPER_TYPE_MAP.containsKey(source.getClass());
+  }
+  public static void main(String[] args) {
+    assertTrue(PrimitiveTypeUtil.isPrimitiveType(false));
+    assertTrue(PrimitiveTypeUtil.isPrimitiveType(1L));
+    assertFalse(PrimitiveTypeUtil.isPrimitiveType(StringUtils.EMPTY));
+  }
+}
